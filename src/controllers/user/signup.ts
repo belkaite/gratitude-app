c92bed64d6c410dba1be67b2a9ae7aa0408dc1f1
Monika <monika.belkaite@gmail.com>
2025-01-14 23:21:57 +0200

    feat: add signup procedure for user registration

--- src/controllers/user/signup.ts
+++ src/controllers/user/signup.ts
@@ -1,3 +1,26 @@
-// import type { publicProcedure } from "@server/trpc";
+import { publicProcedure } from '@server/trpc'
+import provideRepos from '@server/trpc/provideRepos'
+import { userRepository } from '@server/repositories/userRepository'
+import { userSchema } from '@server/entities/user'
 
-// export default publicProcedure.
+export default publicProcedure
+  .use(provideRepos({ userRepository }))
+  .input(
+    userSchema.pick({
+      email: true,
+      password: true,
+      firstName: true,
+      lastName: true,
+    })
+  )
+  .mutation(async ({ input, ctx: { repos } }) => {
+    const { email, password, firstName, lastName } = input
+    const newUser = await repos.userRepository.create({
+      email,
+      password,
+      firstName,
+      lastName,
+    })
+
+    return { id: newUser.id }
+  })
