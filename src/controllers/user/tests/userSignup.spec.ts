810807402a20feb809a5b573aff4aeab879d890b
Monika <monika.belkaite@gmail.com>
2025-01-19 14:00:21 +0200

    test: improve test in user signup

--- src/controllers/user/tests/userSignup.spec.ts
+++ src/controllers/user/tests/userSignup.spec.ts
@@ -1,7 +1,9 @@
 import { createCallerFactory } from '@server/trpc'
 import { createTestDatabase } from '@tests/utils/database'
 import { wrapInRollbacks } from '@tests/utils/transactions'
+import { selectAll } from '@tests/utils/records'
 import userRouter from '..'
+import exp from 'constants'
 
 const db = await wrapInRollbacks(createTestDatabase())
 
@@ -22,4 +24,18 @@ it('should save the user', async () => {
 
   // assert
   expect(response).toEqual({ id: expect.any(Number) })
+
+  const [userCreated] = await selectAll(db, 'user', (eb) =>
+    eb('email', '=', user.email)
+  )
+
+  expect(userCreated).toMatchObject({
+    id: expect.any(Number),
+    email: 'koldunas@domain.com',
+    password: 'password123**',
+    firstName: 'Dumas',
+    lastName: 'Koldunas',
+  })
+
+  expect(response).toEqual({id: userCreated.id})
 })
